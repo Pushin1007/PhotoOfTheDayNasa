8beeb0b1ca8bbc2bc56e26cbbc102a994e665ed9
Pushin <geiby@mail.ru>
2021-12-15 21:42:54 +0300

    less 1

--- app/src/main/java/com/pd/photo_of_the_day_nasa/view/MainActivity.kt
+++ app/src/main/java/com/pd/photo_of_the_day_nasa/view/MainActivity.kt
@@ -3,10 +3,15 @@ package com.pd.photo_of_the_day_nasa.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.pd.photo_of_the_day_nasa.R
+import com.pd.photo_of_the_day_nasa.view.picture.PictureOfTheDayFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, PictureOfTheDayFragment.newInstance()).commit()
+        }
     }
 }
