93e179ee07d075659dfee952d65a6c4bb8e338fc
Pushin <geiby@mail.ru>
2021-12-18 00:36:20 +0300

    Работа с нижней навигацией

--- app/src/main/java/com/pd/photo_of_the_day_nasa/view/picture/BottomNavigationDrawerFragment.kt
+++ app/src/main/java/com/pd/photo_of_the_day_nasa/view/picture/BottomNavigationDrawerFragment.kt
@@ -27,13 +27,16 @@ class BottomNavigationDrawerFragment : BottomSheetDialogFragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.navigationView.setNavigationItemSelectedListener { menuItem ->
-
+// обрабатываем нажатия
             when (menuItem.itemId) {
                 R.id.navigation_one -> {
                     Toast.makeText(context,"1", Toast.LENGTH_SHORT).show()
                 }
                 R.id.navigation_two -> {
                     Toast.makeText(context,"2", Toast.LENGTH_SHORT).show()
+                }
+                R.id.navigation_three -> {
+                    Toast.makeText(context,"3", Toast.LENGTH_SHORT).show()
                 }
             }
 
