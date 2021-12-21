5839e150e0c82089157fa41b365cc31e2a8f4977
Pushin <geiby@mail.ru>
2021-12-21 23:43:39 +0300

    Делаю заглушки экранов марма, земли и луны

---
+++ app/src/main/java/com/pd/photo_of_the_day_nasa/view/api_nasa/EarthFragment.kt
@@ -0,0 +1,19 @@
+package com.pd.photo_of_the_day_nasa.view.api_nasa
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.pd.photo_of_the_day_nasa.R
+
+class EarthFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_earth,container,false)
+    }
+}
